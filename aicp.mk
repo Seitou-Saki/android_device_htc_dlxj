8ebde01fc23a3903ba41e143c683748cf9c2c215
Seitou Saki <seitou.shrine@gmail.com>
2017-04-26 06:30:42 +0900

    AICP-ify

--- aicp.mk
+++ aicp.mk
@@ -1,6 +1,7 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/configs/bootanimation.mk
 
 # Release name
 PRODUCT_RELEASE_NAME := dlxj
@@ -9,14 +10,14 @@ PRODUCT_RELEASE_NAME := dlxj
 PRODUCT_PACKAGE_OVERLAYS += device/htc/dlxj/overlay
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/configs/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/dlxj/full_dlxj.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dlxj
-PRODUCT_NAME := cm_dlxj
+PRODUCT_NAME := aicp_dlxj
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := HTL21
 PRODUCT_MANUFACTURER := HTC
@@ -27,3 +28,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=htc/verizon_wwe/dlx:4.4.2/KOT49H/322791.1:user/release-keys \
     PRIVATE_BUILD_DESC="4.09.605.1 CL322791 release-keys"
 
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Seitou Saki(Unofficial)"
